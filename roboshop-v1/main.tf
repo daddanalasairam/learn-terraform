213e7838ed8df893dcb1b39821e7653929a800f3
daddanalasairam <sairamdaddanala11@gmail.com>
2026-04-01 23:02:46 +0530

    Terraform Commit

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -6,3 +6,93 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+
+resource "aws_instance" "mongodb" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_instance" "user" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_instance" "cart" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_instance" "shipping" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_instance" "payment" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_instance" "dispatch" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
+  tags = {
+    Name = "dispatch"
+  }
+}

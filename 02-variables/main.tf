a9a5845f646ae2164e500d63c16972582a1c4a19
daddanalasairam <sairamdaddanala11@gmail.com>
2026-04-02 23:54:55 +0530

    Terraform Variable Commit

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -2,6 +2,13 @@ variable "URL" {
   default = "example.com"
 }
 
+# Access Variable
 output "URL" {
   value = var.URL
 }
+
+# Variable is a combination of some other string. Then we have to use ${}
+output "URL1" {
+  value = "URL - ${var.URL}"
+}
+

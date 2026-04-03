baf21cb5974b9e110ce9dfb7c0bea914d5111210
daddanalasairam <sairamdaddanala11@gmail.com>
2026-04-03 23:50:28 +0530

    Terraform For-each Loops Commit

--- 03-loops/for_each.tf
+++ 03-loops/for_each.tf
@@ -10,15 +10,3 @@ resource "null_resource" "fruits" {
   for_each = var.fruits
 }
 
-variable "server_names" {
-  default = ["app1", "app2", "app3"]
-}
-
-resource "aws_instance" "servers" {
-  count         = length(var.server_names)
-  ami           = "ami-12345678"
-  instance_type = "t2.micro"
-  tags = {
-    Name = var.server_names[count.index]
-  }
-}

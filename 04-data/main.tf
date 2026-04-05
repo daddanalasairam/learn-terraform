124c8bfa4b3ddc296721dd4df38319622c5df757
daddanalasairam <sairamdaddanala11@gmail.com>
2026-04-05 23:31:09 +0530

    Terraform Data-Source Loops Commit

--- 04-data/main.tf
+++ 04-data/main.tf
@@ -1,8 +1,12 @@
 resource "aws_instance" "test" {
-  ami = "ami-0b4f379183e5706b9"
+  ami = data.aws.ami.ami.id
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
   tags = {
     Name = "test"
   }
 }
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "RHEL-9-DevOps-Practice"
+  owners           = ["973714476881"]
